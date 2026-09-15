4ee9780397ccefd7ff5554e8ee891e40be6a5909
Boris Damevin <bdamevin@frontfactory.dev>
2026-09-15 23:36:30 +0200

    fix(config): name config object following pluginName/purpose convention

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import stylistic from '@stylistic/eslint-plugin';
 export default [
     js.configs.recommended,
     {
-        name: '@front-factory/eslint-config',
+        name: 'front-factory/recommended',
         languageOptions: {
             ecmaVersion: 'latest',
             sourceType: 'module',
